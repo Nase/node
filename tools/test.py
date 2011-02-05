fce6c68d7475e7c2df8621410b50dbfb743b68fa
Mark Cahill <mark@tiemonster.info>
2011-02-05 15:47:34 -0500

    This messed stuff up. Reverting.
    This reverts commit f5205137b08cbff379452590398d0b8ebe5c3f97.

--- tools/test.py
+++ tools/test.py
@@ -144,9 +144,7 @@ def EscapeCommand(command):
       parts.append('"%s"' % part)
     else:
       parts.append(part)
-  new_command = " ".join(parts)
-  # Stetson edit, trying to get path converted properly
-  return new_command.replace('\\', '\\\\')
+  return " ".join(parts)
 
 
 class SimpleProgressIndicator(ProgressIndicator):
@@ -578,7 +576,9 @@ VARIANT_FLAGS = [[]]
 class TestRepository(TestSuite):
 
   def __init__(self, path):
-    normalized_path = abspath(path)
+    # Stetson edits
+    normalized_path = abspath(path).replace('\\', '\\\\')
+    # assert False, normalized_path
     super(TestRepository, self).__init__(basename(normalized_path))
     self.path = normalized_path
     self.is_loaded = False
@@ -671,11 +671,12 @@ class Context(object):
       name = 'build/default/node'
 
     if utils.IsWindows() and not name.endswith('.exe'):
-      name = os.path.abspath(name + '.exe')
+	  # Stetson edit
+      name = os.path.abspath(name + '.exe').replace('\\', '\\\\')
     return name
 
   def GetVmCommand(self, testcase, mode):
-    assert Flase, [self.GetVm(mode)] + self.GetVmFlags(testcase, mode)
+    return [self.GetVm(mode)] + self.GetVmFlags(testcase, mode)
 
   def GetVmFlags(self, testcase, mode):
     return testcase.variant_flags + FLAGS[mode]
